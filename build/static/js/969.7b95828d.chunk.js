96541abdfc025372d64d7716188bd3507048c214
uzuizan <153780814+uzuizan@users.noreply.github.com>
2026-08-14 05:39:14 +0800

    last one

---
+++ build/static/js/969.7b95828d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpawcruz_veterinary_management_system=self.webpackChunkpawcruz_veterinary_management_system||[]).push([[969],{90969(e,t,r){r.r(t),r.d(t,{default:()=>s});r(65043);var a=r(97152),n=r(70579);function s(e){let t=e.profile;return(0,n.jsx)(a.A,{profile:t,title:"Staff Profile"})}}}]);
+//# sourceMappingURL=969.7b95828d.chunk.js.map
